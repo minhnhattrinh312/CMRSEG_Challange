7c5764afb91b08671c9395107598e3cece5bc45b
Minh Nhat Trinh <nhatthdyh@gmail.com>
2026-09-08 10:01:03 +0100

    add code

--- src/data_preprocessing/CMR2npz.py
+++ src/data_preprocessing/CMR2npz.py
@@ -32,8 +32,8 @@ with initialize_config_dir(version_base=None, config_dir=str(config_dir)):
     cfg = compose(config_name="data_config")
 
 # loop through all folder in data config
-# for cmr_type in cfg.CMR_MULTI.keys():
-for cmr_type in ["LGE_MULTI"]:
+for cmr_type in cfg.CMR_MULTI.keys():
+# for cmr_type in ["LGE_MULTI"]:
     os.makedirs(os.path.join(save_dir, cmr_type), exist_ok=True)
     for cmr_view in cfg.CMR_MULTI[cmr_type].keys():
         os.makedirs(os.path.join(save_dir, cmr_type, cmr_view), exist_ok=True)
